ffad2f91b43243af8ccb8c23c84a0fcb13e72997
Kirdan <p.kovalenko92@gmail.com>
2026-07-22 16:30:26 +0300

    step 5: Created empty course list component and styles, added new constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { mockedCoursesList } from "./constants";
 import Header from "./components/Header/Header";
+import EmptyCourseList from "./components/EmptyCourseList/EmptyCourseList";
 
 function App() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            {/* {mockedCoursesList.length === 0 && <EmptyCourseList />} */}
+            <EmptyCourseList />
+        </>
+    );
 }
 
 export default App;
