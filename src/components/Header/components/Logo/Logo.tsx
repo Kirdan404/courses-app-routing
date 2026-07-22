bb0e82b0361a054870a95eac30c5e5c85c935bcb
Kirdan <p.kovalenko92@gmail.com>
2026-07-22 13:35:23 +0300

    step 3: Created header with logo and logout button, added first const

--- src/components/Header/components/Logo/Logo.tsx
+++ src/components/Header/components/Logo/Logo.tsx
@@ -1,7 +1,8 @@
 import logo from "../../../../assets/logo.png";
+import "./Logo.css";
 
 function Logo() {
-    return <img src={logo} alt="Courses logo" />
+    return <img className="logo" src={logo} alt="Courses logo" />;
 }
 
 export default Logo;
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const USER_NAME_HARRY = "Harry Potter";
+export const LOGOUT_BUTTON_TEXT = "Logout";
