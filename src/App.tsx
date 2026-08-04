70cdda6c2dc78f5a1cb73657e2f66f023792444d
Kirdan <p.kovalenko92@gmail.com>
2026-08-04 12:36:26 +0300

    feat: add application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Courses from "./components/Courses/Courses";
+import Login from "./components/Login/Login";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Registration from "./components/Registration/Registration";
 
 function App() {
     return (
-        <>
-            <Header />
-            <Courses />
-        </>
+        <BrowserRouter>
+            <Header showUserActions={Boolean(localStorage.getItem("token"))} />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/registration" element={<Registration />} />
+                <Route
+                    path="/courses"
+                    element={
+                        <PrivateRoute>
+                            <Courses />
+                        </PrivateRoute>
+                    }
+                />
+                <Route path="/" element={<Navigate to="/courses" replace />} />
+                <Route path="*" element={<Navigate to="/courses" replace />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
